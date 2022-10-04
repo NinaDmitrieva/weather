5f20cb96f91a2883659dba215a82e15a21386805
Nina Dmitrieva <nina.dmitrieva.spb>
2022-10-04 13:46:57 +0300

    верстка попапа

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import {Route, Routes} from 'react-router-dom';
 import { Home } from '../src/pages/Home/Home';
 import { MonchStatistics } from '../src/pages/MonchStatistics/MonchStatistics';
 import { Header } from './shered/Header/Header';
+import { Popup } from './shered/Popup/Popup';
 
 
 function App() {
   return (
+  <div className="global-container">
+      <Popup />
     <div className="container">
       <Header />
      < Routes>
@@ -14,6 +17,7 @@ function App() {
         <Route path='/montch-statistics'  element={<MonchStatistics />}/>
      </Routes>
     </div>
+  </div>
   );
 }
 
