0a5f0e0bd86154345a4b3bddd96e2690bb7354d2
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2022-11-09 15:25:37 +0300

    fix

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,9 +1,9 @@
-import axios, { AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
 import api from "../axios";
 import { Weather } from "../store/types/types";
 
 export class WeatherService {
     static getCurrentWeather(city: string): Promise<AxiosResponse<Weather>> {
-        return api.get<Weather>(`/weather?lat={lat}&lon=${city}`);
+        return api.get<Weather>(`/weather?q=${city}`);
     }
 };
